acdaef44d4a5d27540819a778d8f9013851db7a5
Benjamin Eceiza <benjaceiza400@gmail.com>
2025-08-24 20:36:40 -0300

    slider-home

---
+++ js/headerAnimation.js
@@ -0,0 +1,33 @@
+
+window.addEventListener('scroll', function () {
+
+    let headerOne = document.getElementById("header");
+    let nav = document.getElementById("container-fluid");
+
+    let posicionObj1 = headerOne.getBoundingClientRect().top;
+    let tamanoPantallaTextos = window.pageYOffset;
+
+
+
+
+    if (posicionObj1 = tamanoPantallaTextos) {
+        headerOne.style.backgroundColor = '#133222'
+        headerOne.style.transition = "all .5s"
+        nav.style.backgroundColor = '#133222'
+        nav.style.transition = "all .5s"
+
+
+    }
+
+
+    if (tamanoPantallaTextos == 0) {
+        headerOne.style.backgroundColor = 'transparent'
+        headerOne.style.transition = "all .5s"
+        nav.style.backgroundColor = 'transparent'
+        nav.style.transition = "all .5s"
+    }
+
+
+
+
+})
