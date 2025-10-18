99052c230dc21b95d14f3dfdc525241ee766a563
Benjamin Eceiza <benjaceiza400@gmail.com>
2025-10-18 12:36:27 -0300

    .

--- src/components/views/home/Home.jsx
+++ src/components/views/home/Home.jsx
@@ -37,52 +37,52 @@ const Home = () => {
         <>
 
             <main className="main-index">
-                <div className="fondo-negro-main">
-                    <SliderProyectos/>
-                    <section className="section-frase">
-                        <div className="contenedor-frase">
-                            <p className="frase">CONSTRUIMOS</p>
-                            <p className="frase">|</p>
-                            <p className="frase">DESARROLLAMOS</p>
-                            <p className="frase">|</p>
-                            <p className="frase">INVERTIMOS</p>
 
-                        </div>
-                    </section>
-                    <section className="section-frase-mob pantalla">
-                        <div className="contenedor-frase-mob">
-                            <ul className="lista-frase">
-                                <li className="item-frase">
-                                    <img src={check} alt="" />
-                                    <p className="frase-mob">CONSTRUIMOS</p>
-                                </li>
-                                <li className="item-frase">
-                                    <img src={check} alt="" />
-                                    <p className="frase-mob">DESARROLLAMOS</p>
-                                </li>
-                                <li className="item-frase">
-                                    <img src={check} alt="" />
-                                    <p className="frase-mob">INVERTIMOS</p>
-                                </li>
-                            </ul>
-                        </div>
-                    </section>
-                    <div className="contenedor-title">
-                        <h2 className="sub">ELEGÍ TU LUGAR EN TERRALOTEOS</h2>
+                <SliderProyectos />
+                <section className="section-frase">
+                    <div className="contenedor-frase">
+                        <p className="frase">CONSTRUIMOS</p>
+                        <p className="frase">|</p>
+                        <p className="frase">DESARROLLAMOS</p>
+                        <p className="frase">|</p>
+                        <p className="frase">INVERTIMOS</p>
+
                     </div>
-                    <Categorias />
-                    <CategoriasMob />
-                    <div className="contenedor-title">
-                        <h2 className="sub">NOSOTROS</h2>
+                </section>
+                <section className="section-frase-mob pantalla">
+                    <div className="contenedor-frase-mob">
+                        <ul className="lista-frase">
+                            <li className="item-frase">
+                                <img src={check} alt="" />
+                                <p className="frase-mob">CONSTRUIMOS</p>
+                            </li>
+                            <li className="item-frase">
+                                <img src={check} alt="" />
+                                <p className="frase-mob">DESARROLLAMOS</p>
+                            </li>
+                            <li className="item-frase">
+                                <img src={check} alt="" />
+                                <p className="frase-mob">INVERTIMOS</p>
+                            </li>
+                        </ul>
                     </div>
-                    <Numeros />
-                    <div className="contenedor-title">
-                        <h2 className="sub">DONDE NOS ENCONTRAMOS</h2>
-                    </div>
-                    <Mapas />
-                    <SectionContacto />
-                    <SectionContactoMob />
+                </section>
+                <div className="contenedor-title">
+                    <h2 className="sub">ELEGÍ TU LUGAR EN TERRALOTEOS</h2>
                 </div>
+                <Categorias />
+                <CategoriasMob />
+                <div className="contenedor-title">
+                    <h2 className="sub">NOSOTROS</h2>
+                </div>
+                <Numeros />
+                <div className="contenedor-title">
+                    <h2 className="sub">DONDE NOS ENCONTRAMOS</h2>
+                </div>
+                <Mapas />
+                <SectionContacto />
+                <SectionContactoMob />
+
             </main>
         </>
     )
